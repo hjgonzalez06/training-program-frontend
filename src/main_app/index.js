e0f3d82024e57c431deca534bc1249a62f47e6ca
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-03-16 11:42:51 -0400

    Implement currentUser query

--- src/main_app/index.js
+++ src/main_app/index.js
@@ -2,16 +2,22 @@ import { loader } from 'graphql.macro'
 import { useQuery } from '@apollo/client'
 
 import client from 'services/graphql/client'
+import SignUp from './components/SignUp'
 
-const exampleQuery = loader('./graphql/currentUser.graphql')
+const currentUserQuery = loader('./graphql/currentUser.graphql')
 
 function App () {
-  const { loading, error, data } = useQuery(exampleQuery, { client })
+  const { loading, error, data } = useQuery(currentUserQuery, { client })
 
-  console.log(loading, error, data)
+  const showCurrentUser = () => {
+    if (error) return alert(JSON.stringify(error, null, 2))
+    return alert(JSON.stringify(data, null, 2))
+  }
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <button onClick={() => showCurrentUser()}>See current user</button>
+    </>
   )
 }
 
