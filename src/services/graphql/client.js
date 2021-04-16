9780277bec506cfca492e401a7bfcd4ea8ab4d1c
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-04-16 17:17:34 -0400

    Implement Access Token

--- src/services/graphql/client.js
+++ src/services/graphql/client.js
@@ -1,8 +1,42 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, concat } from '@apollo/client'
+
+import { getAccessToken, setAccessToken } from '../authentication/access_token'
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = getAccessToken()
+  
+  if (token) {
+    operation.setContext({
+      credentials: 'include',
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+  }
+  
+  return forward(operation).map(result => {
+    const { response } = operation.getContext()
+    const authorizationHeaderValue = response?.headers.get('authorization')
+    
+    try{
+      const token = authorizationHeaderValue.replace('Bearer ','')
+      setAccessToken(token)
+    } catch (e) {
+      console.log('Access token is undefined')
+    }
+  
+    return result
+  })
+})
+​
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3001/graphql'
+})
+
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink)
 })
 
 export default client
